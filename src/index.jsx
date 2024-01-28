965660f763030d85d9612ce9122e41876b0333b1
Agrey Tosira <agreytosira@gmail.com>
2024-01-28 13:05:21 +0700

    init: setup sass

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 // import style
-import './styles/style.css';
+import './styles/global.scss';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<h1>Hello, World!</h1>);
