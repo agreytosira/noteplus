f260523de222f1242777c5a668f3dc154feef74a
Agrey Tosira <agreytosira@gmail.com>
2024-01-28 14:56:23 +0700

    feat: finish seperate active note and archived note

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import NoteItem from './NoteItem';
 
-function NoteList({ notes }) {
-    return (
-        <div className='note-list'>
-            {notes.map((note) => (
-                <NoteItem {...note} key={note.id} id={note.id} />
-            ))}
-        </div>
-    );
+function NoteList({ notes, isArchived }) {
+    return <div className='note-list'>{isArchived ? notes.filter((note) => note.archived).map((note) => <NoteItem {...note} key={note.id} id={note.id} />) : notes.map((note) => <NoteItem {...note} key={note.id} id={note.id} />)}</div>;
 }
 
 export default NoteList;
