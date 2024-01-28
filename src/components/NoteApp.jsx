85dad6faf9a4c84c51ec47e4911096414c6a6e6e
Agrey Tosira <agreytosira@gmail.com>
2024-01-29 03:50:42 +0700

    style: increase note list margin bottom

--- src/components/NoteApp.jsx
+++ src/components/NoteApp.jsx
@@ -1,81 +1,80 @@
-import React, { Component } from 'react';
-import getInitialData from './../utils/data';
-import FloatingButton from './FloatingButton';
-import Header from './Header';
-import NoteAddModal from './NoteAddModal';
-import NoteContainer from './NoteContainer';
+import React, { Component } from 'react'
+import getInitialData from './../utils/data'
+import FloatingButton from './FloatingButton'
+import Header from './Header'
+import NoteAddModal from './NoteAddModal'
+import NoteContainer from './NoteContainer'
 
 export class NoteApp extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            notes: getInitialData(),
-            isModalOpen: false
-        };
-
-        this.openModalHandler = this.openModalHandler.bind(this);
-        this.onAddNoteHandler = this.onAddNoteHandler.bind(this);
-        this.toggleArchivedHandler = this.toggleArchivedHandler.bind(this);
-        this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  constructor(props) {
+    super(props)
+    this.state = {
+      notes: getInitialData(),
+      isModalOpen: false
     }
 
-    openModalHandler() {
-        this.setState((prevState) => ({
-            isModalOpen: !prevState.isModalOpen
-        }));
-    }
+    this.openModalHandler = this.openModalHandler.bind(this)
+    this.onAddNoteHandler = this.onAddNoteHandler.bind(this)
+    this.toggleArchivedHandler = this.toggleArchivedHandler.bind(this)
+    this.onDeleteHandler = this.onDeleteHandler.bind(this)
+  }
 
-    onAddNoteHandler({ title, body }) {
-        this.setState((prevState) => {
-            return {
-                notes: [
-                    ...prevState.notes,
-                    {
-                        id: +new Date(),
-                        title,
-                        body,
-                        createdAt: +new Date(),
-                        archived: false
-                    }
-                ]
-            };
-        });
-    }
+  openModalHandler() {
+    this.setState((prevState) => ({
+      isModalOpen: !prevState.isModalOpen
+    }))
+  }
 
-    onDeleteHandler(id) {
-        const notes = this.state.notes.filter((note) => note.id !== id);
-        this.setState({ notes });
-    }
+  onAddNoteHandler({ title, body }) {
+    this.setState((prevState) => {
+      return {
+        notes: [
+          ...prevState.notes,
+          {
+            id: +new Date(),
+            title,
+            body,
+            createdAt: +new Date(),
+            archived: false
+          }
+        ]
+      }
+    })
+  }
 
-    toggleArchivedHandler(id) {
-        this.setState((prevState) => {
-            const updatedNotes = prevState.notes.map((note) => {
-                if (note.id === id) {
-                    return {
-                        ...note,
-                        archived: !note.archived
-                    };
-                }
-                return note;
-            });
+  onDeleteHandler(id) {
+    const notes = this.state.notes.filter((note) => note.id !== id)
+    this.setState({ notes })
+  }
 
-            return {
-                notes: updatedNotes
-            };
-        });
-    }
+  toggleArchivedHandler(id) {
+    this.setState((prevState) => {
+      const updatedNotes = prevState.notes.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            archived: !note.archived
+          }
+        }
+        return note
+      })
 
-    render() {
-        return (
-            <>
-                <Header />
-                <NoteContainer notes={this.state.notes} toggleArchived={this.toggleArchivedHandler} onDelete={this.onDeleteHandler} />
-                <FloatingButton handler={this.openModalHandler} />
-                {this.state.isModalOpen && <NoteAddModal addNote={this.onAddNoteHandler} closeHandler={this.openModalHandler} />}
-            </>
-        );
-    }
+      return {
+        notes: updatedNotes
+      }
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <NoteContainer notes={this.state.notes} toggleArchived={this.toggleArchivedHandler} onDelete={this.onDeleteHandler} />
+        <FloatingButton handler={this.openModalHandler} />
+        {this.state.isModalOpen && <NoteAddModal addNote={this.onAddNoteHandler} closeHandler={this.openModalHandler} />}
+      </>
+    )
+  }
 }
 
-export default NoteApp;
+export default NoteApp
