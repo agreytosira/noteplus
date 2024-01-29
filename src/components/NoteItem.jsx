7fde05dfcb202ab8a3abc47d2e7cef19a48c4717
Agrey Tosira <agreytosira@gmail.com>
2024-01-29 09:08:35 +0700

    fix: change button value when archived

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import showFormattedDate from '../utils/format';
+import React from 'react'
+import showFormattedDate from '../utils/format'
 
-function NoteItem({ id, title, body, createdAt, toggleArchived, onDelete }) {
-    return (
-        <div className='note-item' id={id}>
-            <div className='note-item__body'>
-                <h3 className='note-item__title'>{title}</h3>
-                <p className='note-item__date'>{showFormattedDate(createdAt)}</p>
-                <p className='note-item__content'>{body}</p>
-            </div>
-            <div className='note-item__action'>
-                <button onClick={() => onDelete(id)}>Hapus</button>
-                <button onClick={() => toggleArchived(id)}>Arsipkan</button>
-            </div>
-        </div>
-    );
+function NoteItem({ id, title, body, createdAt, toggleArchived, onDelete, archived }) {
+  return (
+    <div className='note-item' id={id}>
+      <div className='note-item__body'>
+        <h3 className='note-item__title'>{title}</h3>
+        <p className='note-item__date'>{showFormattedDate(createdAt)}</p>
+        <p className='note-item__content'>{body}</p>
+      </div>
+      <div className='note-item__action'>
+        <button onClick={() => onDelete(id)}>Hapus</button>
+        <button onClick={() => toggleArchived(id)}>{archived ? 'Pindahkan' : 'Arsipkan'}</button>
+      </div>
+    </div>
+  )
 }
 
-export default NoteItem;
+export default NoteItem
