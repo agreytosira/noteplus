53b7fb577f9e4f485ea059a7e74a1f4169fcb334
Agrey Tosira <agreytosira@gmail.com>
2024-02-02 19:43:49 +0700

    style: update back to homepage button

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { getNote } from '../utils/data';
 import { Link } from 'react-router-dom';
 import { showFormattedDate } from '../utils/format';
@@ -8,17 +8,22 @@ import FloatingButton from '../components/FloatingButton';
 function Detail({ onDelete, onArchive, onUnarchive }) {
     const { id } = useParams();
     const { title, body, createdAt, archived } = getNote(id);
+    const navigate = useNavigate();
+
+    const backToPrevPage = () => {
+        navigate(-1);
+    };
 
     return (
         <>
             <main className='note-detail'>
                 <div className='note-detail__container container'>
-                    <Link to='/' className='btn-link'>
+                    <button className='btn-link' onClick={() => backToPrevPage()}>
                         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
                             <path d='M7.82843 10.9999H20V12.9999H7.82843L13.1924 18.3638L11.7782 19.778L4 11.9999L11.7782 4.22168L13.1924 5.63589L7.82843 10.9999Z'></path>
                         </svg>{' '}
-                        Kembali ke beranda
-                    </Link>
+                        Kembali ke Halaman Utama
+                    </button>
                     <h1>{title}</h1>
                     <span>Dibuat pada {showFormattedDate(createdAt)}</span>
                     <p>{body}</p>
