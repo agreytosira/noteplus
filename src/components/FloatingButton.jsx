00fb38f33aced001b4c46e8f756d6a07683ff81d
Agrey Tosira <agreytosira@gmail.com>
2024-02-15 12:01:22 +0700

    style: change add button icon

--- src/components/FloatingButton.jsx
+++ src/components/FloatingButton.jsx
@@ -17,7 +17,7 @@ function FloatingButton({ onCancel, id, title, onAdd, isAddLink, onDelete, onArc
                 {onAdd && (
                     <button onClick={() => onAdd()} title='Tambahkan' className='btn-add'>
                         <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='currentColor'>
-                            <path d='M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z'></path>
+                            <path d='M18 19H19V6.82843L17.1716 5H16V9H7V5H5V19H6V12H18V19ZM4 3H18L20.7071 5.70711C20.8946 5.89464 21 6.149 21 6.41421V20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3ZM8 14V19H16V14H8Z'></path>
                         </svg>
                     </button>
                 )}
